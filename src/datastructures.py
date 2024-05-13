377c07c3a28a572a78d4be568d0e1051d6a45c16
JPbonaldo <159311815+JPbonaldo@users.noreply.github.com>
2024-05-13 20:25:53 +0000

    done

--- src/datastructures.py
+++ src/datastructures.py
@@ -21,15 +21,23 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        if not member['id']:
+            member['id'] = self._generateId()
 
+        self._members.append(member)
+        
     def delete_member(self, id):
         # fill this method and update the return
-        pass
-
+        for member in self._members:
+            if member['id'] == id:
+                self._members = list(filter(lambda member: member['id'] != id, self._members))
+                return {"done": True}
+            
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
